8c4ef1b8bd18ce0281ef2d5ae9f9dc1d6deba644
Xuyang Han <hanxuyang0826@gmail.com>
2021-04-08 02:53:06 -0400

    1. weather-card comonnet ts -> made info into city objects -> temp is consistent with cities 2. change dispaly into flex laywout

--- ClientApp/src/app/weather-card/weather-card.component.ts
+++ ClientApp/src/app/weather-card/weather-card.component.ts
@@ -10,51 +10,44 @@ import { HttpClient } from '@angular/common/http'
 })
 export class WeatherCardComponent{
   Cities: City[] = [];
-  state: string[] = [];
-  temp: number[] = [];
-  maxTemp: number[] = [];
-  minTemp: number[] = [];
-  test: number[] = [];
+  state: object[] = [];
+  temp: object[] = [];
+  maxTemp: object[] = [];
+  minTemp: object[] = [];
+  test: object[] = [];
 
   constructor(public weather: WeatherService, http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    let state_arr: string[] = [];
-    let temp_arr: number[] = [];
-    let maxTemp_arr: number[] = [];
-    let minTemp_arr: number[] = [];
-    let test_arr: number[] = [];
 
     http.get<City[]>(baseUrl + 'api/Cities').subscribe(result => {
       this.Cities = result;
-      for (let i = 0; i < result.length; i++) {
-        this.weather.getWeather(result[i]['name'])
+      for (let city of result) {
+        this.weather.getWeather(city['name'])
           .pipe(first())
           .subscribe((payload) => {
-            state_arr.push(payload.weather[0].main);
-            temp_arr.push(payload.main.temp);
-            maxTemp_arr.push(Math.ceil(payload.main.temp_max));
-            minTemp_arr.push(Math.floor(payload.main.temp_min));
-            test_arr.push(i)
+
+            city['state'] = payload.weather[0].main;
+            city['temp'] = payload.main.temp;
+            city['maxTemp'] = Math.ceil(payload.main.temp_max);
+            city['minTemp'] = Math.floor(payload.main.temp_min);
+            city['test'] = city['name'];
+
           }, error => console.error(error));
       }
     }, error => console.error(error));
 
-    
-    this.state = state_arr;
-    
-    this.temp = temp_arr;
-    
-    this.maxTemp = maxTemp_arr;
-    
-    this.minTemp = minTemp_arr;
-
-    this.test = test_arr;
   }
   
 }  
 
 
 interface City {
-  Id: Number;
-  citesWeather: Object;
+  Id: number;
+  citesWeather: object;
   Name: string;
+
+  state: string;
+  temp: number;
+  maxTemp: number;
+  minTemp: number;
+  test: number;
 }
